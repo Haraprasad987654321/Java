cfcbb125dc54927d2b1c9ab82fb30c8baff4730f
Haraprasad987654321 <rahulrahul59038@gmail.com>
2025-11-09 13:07:22 +0530

    specified the range of the user data input , user can enter only within 0 to 100 , and updated the lowest grade like earlier if i had scored 40 then i was exxplaing me that  i had failed the exam .

--- app01/src/conditionalformating/Gradecalculator.java
+++ app01/src/conditionalformating/Gradecalculator.java
@@ -7,7 +7,9 @@ public class Gradecalculator {
 		System.out.println("Enter the score obtained by you ");
 		Scanner sc = new Scanner(System.in);
 		double x = sc.nextDouble();
-		if (x>90) {
+		if (x<0 || x>100) {
+			System.out.println("You have entered a invalide  score , Please enter a valid score within 0 to 100 ");
+		}else if (x>90) {
 			System.out.println("your grade is  O ");
 		}
 		else if(x>85) {
@@ -25,8 +27,10 @@ public class Gradecalculator {
 			
 		}else if (x>50) {
 			System.out.println("Your grade is D");
+		}else if (x >= 40) {
+			System.out.println("You have passed the exam with marginal score");
 		}else if (x<40) {
-			System.out.println("You have Failed the exam , Try again");
+			System.out.println("You have Failed the exam ,Try to give more time to the subject you will definitely perform very Good ");
 		}
 		
 		
